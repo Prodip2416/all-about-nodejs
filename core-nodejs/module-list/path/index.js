6bcfe496f2f32fec56eeec4afa1b0a8e99948460
pro_dip <prodip.sarker@apsissplutions.com>
2022-01-07 23:09:28 +0600

    path in node

---
+++ core-nodejs/module-list/path/index.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+// const result = path.basename('C:\\temp\\myfile.html');
+// const result = path.win32.basename('C:\\temp\\myfile.html'); // To achieve consistent results
+
+// const result = path.posix.basename('C:\\temp\\myfile.html');
+
+// const result = path.win32.basename("C:\\foo.html"); // foo.html
+// const result = path.win32.basename("C:\\foo.html", ".html"); // foo
+
+// console.log(result);
+
+
+// console.log(process.env.PATH);
+// Prints: 'C:\Windows\system32;C:\Windows;C:\Program Files\node\'
+
+// console.log(process.env.PATH.split(path.delimiter));
+// Returns ['C:\\Windows\\system32', 'C:\\Windows', 'C:\\Program Files\\node\\']
+
+// const dirname = path.dirname('/foo/bar/baz/asdf/index.txt');
+// console.log(dirname)
+
+
+// const extName = path.extname('index.html');// .html
+// const extName = path.extname('index.coffee.md'); // .md
+// console.log(extName)
+
+
+// const parse = path.parse('/home/user/dir/file.txt');
+// return value 
+// {
+//     root: '/',
+//     dir: '/home/user/dir',
+//     base: 'file.txt',
+//     ext: '.txt',
+//     name: 'file'
+//   }
+// console.log(parse)
+
