a2267c997f08ed60032de310f1ec8b9a703d9b04
Prodip <prodip.sarker.cse@gmail.com>
2025-03-11 22:26:08 +0600

    dependency injection and documentation setup

--- nestjs/nestjs-intro/src/users/users.controller.ts
+++ nestjs/nestjs-intro/src/users/users.controller.ts
@@ -14,31 +14,31 @@ import {
   Req,
   ValidationPipe,
 } from '@nestjs/common';
-import { CreateUserDTO, GetUserDTO } from './users.dto';
+import { CreateUserDTO, GetUserDTO, PatchUserDTO } from './users.dto';
+import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UserService) {}
+
   @Get('{/:id}')
   public getUsers(
-    @Param() getuserDTO: GetUserDTO,
-    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number | undefined,
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number | undefined,
-  ): string {
-    console.log(getuserDTO);
-    console.log(limit);
-    console.log(page);
-    return 'you sent a get request for fetch all users.';
+    @Param() getUserDTO: GetUserDTO,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe)
+    limit: number | undefined,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe)
+    page: number | undefined,
+  ): any {
+    return this.userService.findAll(getUserDTO, limit, page);
   }
   @Post()
-  public createUser(
-    @Body() createUserDTO: CreateUserDTO,
-  ): string {
+  public createUser(@Body() createUserDTO: CreateUserDTO): string {
     console.log(createUserDTO instanceof CreateUserDTO);
     return 'you sent a get request for fetch all users.';
   }
-  @Patch()
-  public updateUser(): string {
-    return 'you sent a get request for fetch all users.';
+  @Patch('/:id')
+  public updateUser(@Body() patchUserDTO: PatchUserDTO): PatchUserDTO {
+    return patchUserDTO;
   }
   @Delete()
   public deleteUser(): string {
