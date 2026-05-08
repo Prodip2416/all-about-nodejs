35c2f3fa95730a4e165a3e1f424df15fa080175a
Prodip <prodip.sarker.cse@gmail.com>
2026-05-08 23:08:35 +0600

    added dashboard page

---
+++ nextcash-course/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="max-w-screen-xl mx-auto py-10">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+    </div>
+  );
+}
